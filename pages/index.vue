b08af2251abb0b464ce0d495881c5518781ec63b
Krystian Fras <krystian@fras.me>
2020-03-27 17:13:03 +0100

    Add article list

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,52 @@
 <template>
-  <section class="section">
-    remove this line and add iteration on products
-  </section>
+  <div>
+    <div class="section">
+      <main class="container">
+        <div class="columns is-multiline">
+          <div
+            v-for="article in articles"
+            :key="article.id"
+            class="column is-3"
+          >
+            <Card :article="article" />
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
 </template>
 
 <script>
+import Card from '~/components/homepage/Card'
+
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+
+  components: {
+    Card
+  },
+
+  data: () => ({
+    articles: [
+      {
+        id: 'article1',
+        slug: 'wstep-do-nuxt-czesc-1',
+        imgURL: 'https://nuxtjs.org/logos/nuxt-icon-white.png',
+        uploadAt: '2020-02-20',
+        author: 'kfras',
+        title: 'Wstep do nuxt czesc 1',
+        desc: 'To pierwszy artykuł z serii artykułów'
+      },
+      {
+        id: 'article2',
+        slug: 'wstep-do-nuxt-czesc-2',
+        imgURL: 'https://nuxtjs.org/logos/nuxt-icon-white.png',
+        uploadAt: '2020-02-20',
+        author: 'kfras',
+        title: 'Wstep do nuxt czesc 2',
+        desc: 'To drugi artykuł z serii artykułów'
+      }
+    ]
+  })
 }
 </script>
