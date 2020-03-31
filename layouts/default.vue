e6f1487f37cb2a6a0b0050d83813e24e4663d462
Krystian Fras <krystian@fras.me>
2020-03-31 17:12:01 +0200

    Add layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+    <TheHeader />
     <nuxt />
+    <TheFooter />
   </div>
 </template>
+
+<script>
+import TheHeader from '~/components/TheHeader'
+import TheFooter from '~/components/TheFooter'
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter
+  }
+}
+</script>
