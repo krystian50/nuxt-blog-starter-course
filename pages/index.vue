58d22e240c1aa27944a719ee60817e0ac7835fe2
Krystian Fras <krystian@fras.me>
2020-03-31 15:02:50 +0200

    removed bulma class

--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,26 @@
 <template>
   <div>
-    <div class="section">
-      <main class="container">
-        <div class="columns is-multiline">
-          <div
-            v-for="article in articles"
-            :key="article.id"
-            class="column is-3"
-          >
-            <Card :article="article" />
-          </div>
-        </div>
-      </main>
+    <div v-for="article in articles" :key="article.id">
+      <nuxt-link :to="`posty/${article.slug}`">{{ article.title }}</nuxt-link>
     </div>
   </div>
 </template>
 
 <script>
-import Card from '~/components/homepage/Card'
-
 export default {
   name: 'HomePage',
-
-  components: {
-    Card
-  },
 
   data: () => ({
     articles: [
       {
         id: 'article1',
         slug: 'wstep-do-nuxt-czesc-1',
-        imgURL: 'https://nuxtjs.org/logos/nuxt-icon-white.png',
-        uploadAt: '2020-02-20',
-        author: 'kfras',
-        title: 'Wstep do nuxt czesc 1',
-        desc: 'To pierwszy artykuł z serii artykułów'
+        title: 'Wstep do nuxt czesc 1'
       },
       {
         id: 'article2',
         slug: 'wstep-do-nuxt-czesc-2',
-        imgURL: 'https://nuxtjs.org/logos/nuxt-icon-white.png',
-        uploadAt: '2020-02-20',
-        author: 'kfras',
-        title: 'Wstep do nuxt czesc 2',
-        desc: 'To drugi artykuł z serii artykułów'
+        title: 'Wstep do nuxt czesc 2'
       }
     ]
   })
